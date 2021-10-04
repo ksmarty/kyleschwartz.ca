a876082792793b1d798bdfa04e3212fa58091a27
Kyle Schwartz <kyleschwartz2000@gmail.com>
2021-10-04 15:33:29 -0400

    Ground 0

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -7,18 +7,7 @@ export default {
 	plugins: [
 		"@snowpack/plugin-svelte",
 		"@snowpack/plugin-postcss",
-		"@snowpack/plugin-dotenv",
-		[
-			"@snowpack/plugin-typescript",
-			{
-				/* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
-				...(process.versions.pnp ? { tsc: "yarn pnpify tsc" } : {}),
-			},
-		],
-	],
-	routes: [
-		/* Enable an SPA Fallback in development: */
-		// {"match": "routes", "src": ".*", "dest": "/index.html"},
+		"@snowpack/plugin-typescript",
 	],
 	optimize: {
 		/* Example: Bundle your final build: */
