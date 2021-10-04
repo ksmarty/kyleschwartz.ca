1aaed31efbf6babf261a1e48ea71205b95b32575
Kyle Schwartz <kyleschwartz2000@gmail.com>
2021-10-04 15:10:22 -0400

    Working with tailwind

---
+++ snowpack.config.mjs
@@ -0,0 +1,36 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+	mount: {
+		public: { url: "/", static: true },
+		src: { url: "/_dist_" },
+	},
+	plugins: [
+		"@snowpack/plugin-svelte",
+		"@snowpack/plugin-postcss",
+		"@snowpack/plugin-dotenv",
+		[
+			"@snowpack/plugin-typescript",
+			{
+				/* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
+				...(process.versions.pnp ? { tsc: "yarn pnpify tsc" } : {}),
+			},
+		],
+	],
+	routes: [
+		/* Enable an SPA Fallback in development: */
+		// {"match": "routes", "src": ".*", "dest": "/index.html"},
+	],
+	optimize: {
+		/* Example: Bundle your final build: */
+		bundle: true,
+	},
+	packageOptions: {
+		/* ... */
+	},
+	devOptions: {
+		tailwindConfig: "./tailwind.config.js",
+	},
+	buildOptions: {
+		/* ... */
+	},
+};
