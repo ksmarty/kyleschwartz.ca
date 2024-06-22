eef1d0d938919a4592e96db3d57f80d1c4f356ee
Kyle Schwartz <kyleschwartz2000@gmail.com>
2024-06-22 23:28:06 +0200

    fix: move analytics to correct location

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
 	import "$lib/css/app.css";
 
-	import { dev } from "$app/environment";
-	import { inject } from "@vercel/analytics";
-
 	import Navbar from "./_components/Navbar.svelte";
 	import Resume from "./_components/Resume.svelte";
 	import Footer from "./_sections/Footer.svelte";
-
-	inject({ mode: dev ? "development" : "production" });
 </script>
 
 <main class="min-h-screen p-3 lg:p-5 flex flex-col !pt-0 print:hidden">
