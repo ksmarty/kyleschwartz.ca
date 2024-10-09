d5433f65189845b7ba026763c9a3124c8d19197b
Kyle Schwartz <kyleschwartz2000@gmail.com>
2024-10-08 21:28:41 -0400

    update resume to fit

--- src/routes/_components/Resume.svelte
+++ src/routes/_components/Resume.svelte
@@ -23,7 +23,7 @@
 
 <div id="Resume" data-theme="light" class="hidden print:block text-base-content">
 	<div class="mb-6">
-		<div class="flex justify-between mb-6">
+		<div class="flex justify-between mb-4">
 			<h1 class="text-4xl font-bold">Kyle Schwartz</h1>
 
 			<div class="flex flex-col justify-between">
